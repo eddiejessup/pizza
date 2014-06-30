0cf7529b154aca1e0491a1ca69112c89b6c57974
Elliot Marsden <elliot.marsden@gmail.com>
2014-06-30 12:08:51 +0100

    Remove unused code

--- src/histo.py
+++ src/histo.py
@@ -1,42 +1,8 @@
-# Pizza.py toolkit, www.cs.sandia.gov/~sjplimp/pizza.html
-# Steve Plimpton, sjplimp@sandia.gov, Sandia National Laboratories
-#
-# Copyright (2005) Sandia Corporation.  Under the terms of Contract
-# DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
-# certain rights in this software.  This software is distributed under 
-# the GNU General Public License.
-
-# histo tool
-
-oneline = "Particle density histogram from a dump"
-
-docstr = """
-h = histo(d)                        d = dump/cdump object
-
-x,y = h.compute('x',N,lo,hi)        compute histogram in dim with N bins
-
-  lo/hi are optional, if not used histo will be over entire box
-"""
-
-# History
-#   12/05, Steve Plimpton (SNL): original version
-
-# ToDo list
-
-# Variables
-#   data = dump object
-
-# Imports and external programs
-
-# Class definition
-
 class histo:
-
-  # --------------------------------------------------------------------
 
   def __init__(self,data):
     self.data = data
-    
+
   # --------------------------------------------------------------------
 
   def compute(self,dim,nbins,lo=None,hi=None):
@@ -46,7 +12,7 @@ class histo:
     else: raise StandardError,"illegal dim value"
 
     y = nbins*[0]
-    
+
     count = 0
     n = flag = 0
     while 1:
@@ -67,19 +33,19 @@ class histo:
 
       delta = (hi-lo) / nbins;
       invdelta = 1.0/delta
-      
+
       for atom in atoms:
         coord = atom[idim]
         ibin = int((coord-lo) * invdelta)
         if ibin < 0 or ibin >= nbins: continue
         y[ibin] += 1
         count += 1
-        
+
       n += 1
 
     x = nbins*[0]
     for i in xrange(nbins): x[i] = (i+0.5)*delta
-      
+
     print "histogram snapshots = ",n
     print "histogram counts (per snap) = %d (%g)" % (count,float(count)/n)
     print "histogram bounds = ",lo,hi
